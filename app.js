4981d7c7ef80e08853a5e6c2fed9245a73e6d9ef
Jano <imhtejano@outlook.com>
2020-06-24 11:06:09 -0500

    first

--- app.js
+++ app.js
@@ -27,8 +27,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', indexRouter);
-app.use('/rieggo/api/mailchimp',require('./routes/mailchimp'))
+app.use('/', indexRouter);
+app.use('/api/mailchimp',require('./routes/mailchimp'))
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- controllers/Mailchimp.js
+++ controllers/Mailchimp.js
@@ -6,7 +6,8 @@ module.exports = {
 			var body = { members: [ { email_address: req.body.email, status: 'pending' } ] }
 			var headers = {'Authorization': 'Basic ' + new Buffer(`rotoplas:5bb7b1f4d71ddb3e028162f78279d0bc-us10`).toString('base64') } 
 			// var headers = {user:'rieggo.com', pass: '5bb7b1f4d71ddb3e028162f78279d0bc-us10' } 
-			var r = await axios.post('https://us10.api.mailchimp.com/3.0/lists/c7cb1ccabf', body, { headers })
+			// var r = await axios.post('https://us10.api.mailchimp.com/3.0/lists/c7cb1ccabf', body, { headers })
+			r = {}
 			res.status( httpStatus.OK ).json({res: r.data})
 		} catch (error) {
 			console.log(error);
